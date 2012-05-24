85a881e395e3adf99ac3e66aeefa4a66ebcc9fdf
Mark Shirley <mshirley@gmail.com>
2012-05-23 18:32:58 -0600

    the wheel turns, and a add to gitignore

--- wheel-of-action.rb
+++ wheel-of-action.rb
@@ -5,14 +5,14 @@ if !File.exists?(todo)
   puts "Please create a file called \'#{todo}\'"
   exit
 else
-  print "Reading #{todo} ..."
+#  print "Reading #{todo} ..."
   begin
     items = File.open(todo).readlines
   rescue
     puts " Failed: An error occured reading #{todo}!"
     exit
   end
-  puts " Success!"
+#  puts " Success!"
 end
 
 puts "There are #{items.count} items."
@@ -22,6 +22,24 @@ if items.count < output
   puts "You have requested #{output} items but you only have #{output} in #{todo}."
 end
 
-print "Generating a random list items ..."
+#puts "Generating a random list of items ..."
 
+counter = 0
+itemlist = []
+while(counter < output)
+  random = rand(1..items.count)
+#  puts "Random number: #{random}"
+  if itemlist.include?(random)
+#    puts "Duplicate detected, break."
+  else
+    itemlist.push(random)
+    counter=counter + 1
+  end
+end
 
+#puts "Using these numbers: #{itemlist}"
+
+puts "Here is your list of things to do!"
+puts ""
+itemlist.each { |i| puts "* #{items[i-1].strip} *" }
+puts ""
